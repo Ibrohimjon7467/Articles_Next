26a817deeca03c1b918b70b2d620379ba1d11c55
ibrohimjon7467 <Ibrohimjon7467@gmail.com>
2024-01-08 21:17:53 +0500

    articlesList

--- src/app/ArticlesList/page.jsx
+++ src/app/ArticlesList/page.jsx
@@ -1,14 +1,18 @@
+// import Image from 'next/image'
 import Link from 'next/link'
 
 function ArticlesList({ articles }) {
+
     return (
         <div className="prose">
             <ul className='list-none p-0 m-0'>
                 {articles.map((article) => {
                     return (
-                        <li key={article.id}>
-                            <h3>{article.title}</h3>
-                            <p>Author: {article.author}</p>
+                        <li className='pb-10' key={article.id}>
+                            {/* <Image src={article.image} width={500} height={500} /> */}
+                            <h2>{article.title}</h2>
+                            <h4>Author: {article.author}</h4>
+                            <p>{article.body}</p>
                             <Link href={`/article/${article.id}`}>Read More</Link>
                         </li>
                     )
